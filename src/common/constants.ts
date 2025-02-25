c5e5cf64cf2211540899c28f557cfd9b8d342a20
Surya-Kumar-D <suryakumar.dhanasekar@gmail.com>
2025-02-25 15:19:27 +0530

    connected to mongodb by intalling mongoose and dotenv, learned more about it

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,3 +1,4 @@
 export default {
-    API_PATH: '/api/v1'
+    API_PATH: '/api/v1',
+    PORT: 3006
 }
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,37 @@
 import Hapi from '@hapi/hapi';
 import todoRoutes from "./modules/todoList/todoRoutes.js";
 import routes from "./routes/routes.js";
+import dotenv from 'dotenv';
+import constants from "./common/constants.js";
+import * as mongoose from "mongoose";
+
+dotenv.config();
+
+const dbPassword = process.env.DBPASSWORD!;
+let mongoURI = process.env.MONGODB!;
+
+// console.log(dbPassword, mongoURI)
+if (dbPassword && mongoURI) {
+    mongoURI = mongoURI.replace('<db_password>', dbPassword);
+    mongoose.connect(mongoURI).then(() => console.log("Connected to MongoDB")).catch((err) => {
+        console.error("Error connecting to mongodb", err);
+        process.exit(1)
+    });
+} else {
+    console.error("Missing Mongo url or db password");
+    process.exit(1)
+}
 
 const init = async () => {
     const server = Hapi.server({
-        port: 3006,
+        port: constants.PORT,
         host: 'localhost'
     });
 
-server.route(routes)
+    server.route(routes)
 
     await server.start();
-    console.log("Server running on port 3000");
+    console.log(`Server running on port ${constants.PORT}`);
 };
 
 process.on('unhandledRejection', (err) => {
@@ -19,4 +39,3 @@ process.on('unhandledRejection', (err) => {
     process.exit(1);
 });
 const result = await init();
-console.log(result);
